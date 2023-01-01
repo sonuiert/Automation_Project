43c88d62409ba0fec486a7bf3683619b1f139036
Shailendra Pandey <shailendra12004@gmail.com>
2023-01-01 23:05:53 +0530

    Update the file to add Cron logs to inventory.html and create Cron scheduler for daily

--- automation.sh
+++ automation.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-## Update the local package.
+## Update the local package
 sudo apt update -y
 
 ## Install Apache2 package
@@ -50,3 +50,27 @@ tar -zcvf /tmp/"$name-httpd-logs-$timestamp".tar /var/log/apache2/*.log
 ## Upload the tar to S3 bucket
 aws s3 cp /tmp/${name}-httpd-logs-${timestamp}.tar s3://${S3bucket}/${name}-httpd-logs-${timestamp}.tar
 
+
+##check if inventory.html is avalable else create it
+
+FILE=/var/www/html/inventory.html
+if [ -f "$FILE" ]; then
+    echo "$FILE exists."
+else 
+	echo  "Log Type\tTime Created\tType\tSize" >/var/www/html/inventory.html
+fi
+
+
+## find the size of the inventory.html file
+filesize=$(du -h /var/www/html/inventory.html | awk '{ print $1}')
+
+## log into inventory.html
+echo  "httpd-logs\t$timestamp\ttar\t$filesize" >> /var/www/html/inventory.html
+
+## Create scheduler for everyday morning 10 AM "00 10 * * * bash /root/Automation_Project/automation.sh"
+iscron=$(crontab -l)
+
+if [[ $iscron == *"automation.sh"* ]]; then
+  echo "Cron is available"
+else echo "00 10 * * * bash /root/Automation_Project/automation.sh" >> crontab -e
+fi
